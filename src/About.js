58c9e36c743f19dbce6642883fdfb227439e38d9
unknown <royharshit812@gmail.com>
2022-10-06 01:19:03 +0530

    Changed about section

--- src/About.js
+++ src/About.js
@@ -1,30 +1,32 @@
 import React from "react";
+import Stack from 'react-bootstrap/Stack';
 
 export default function About()
 {
     return (
 
-        <div class = 'container-fluid d-flex justify-content-around flex-row pt-5 pb-2 about'>
-        
-        <div class = 'd-flex p-2 w-25 about_header'>
-            <h1 className = 'about_header_h1'> <span> welcome to </span> PARK HORIZON . <br /> <p class = 'about_extra'> A fun planet to explore your dreams. </p> </h1>
-        </div>
+        <Stack className = 'h-stack' direction = 'horizontal' gap = {5}>
+            <div className = 'about_header'>
+                <h1 className = 'about_header_h1'> 
+                <span> welcome to </span> PARK HORIZON 
+                <br /> 
+                <p className = 'about_extra'> A fun planet to explore your dreams. </p> 
+                </h1>
+            </div>
+            <div className = 'about_details'>
+                <p> An adventruous fun resort located in Mumbai, India and introduced in 2021.
+                    We aim to make our park the world's largest theme resort in the world. 
+                    We provide excellent facilities, world-class infrastructure and 5-star hospitality.
+                    We have been involved with top-leaders in building this theme park. We have wide 
+                    varities of rides and also have a small waterpark located in the city. As started recently, 
+                    we are trying to introduce our parks in different cities across the globe specially 
+                    America, Canada, Dubai and Australia. We warmly welcome people from anywhere around the 
+                    world. Our cost-friendly ticket prices make our theme park loved to everyone.
+                    So come and enjoy the moment you've never experienced in your life !
 
-        <div class = 'd-flex p-2 w-50 align-items-center about_details'>
-            <p> An adventruous fun resort located in Mumbai, India and introduced in 2021.
-                We aim to make our park the world's largest theme resort in the world. 
-                We provide excellent facilities, world-class infrastructure and 5-star hospitality.
-                We have been involved with top-leaders in building this theme park. We have wide 
-                varities of rides and also have a small waterpark located in the city. As started recently, 
-                we are trying to introduce our parks in different cities across the globe specially 
-                America, Canada, Dubai and Australia. We warmly welcome people from anywhere around the 
-                world. Our cost-friendly ticket prices make our theme park loved to everyone.
-                So come and enjoy the moment you've never experienced in your life !
-
-            </p>
-        </div>
-
-        </div>
+                </p>
+            </div>
+        </Stack>
 
     )
 }
